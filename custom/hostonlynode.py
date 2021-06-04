9a0f54adbaa1fd2753f11fc33e01ec0db47549d9
MarianDubei <dubei@ucu.edu.ua>
2021-06-04 13:33:55 +0300

    fix indents

--- custom/hostonlynode.py
+++ custom/hostonlynode.py
@@ -3,26 +3,26 @@ from mininet.log import info
 
 class HostOnly( Node ): 
 
-	def __init__( self, name, localIntf=Hone, flush=False, inNamespace=False, **params): 
+    def __init__( self, name, localIntf=Hone, flush=False, inNamespace=False, **params): 
 	
-		super( HostOnly, self ).__init__( name, inNamespace, **params )
-		self.locallntf = locallntf 
-		self.flush = flush 
+        super( HostOnly, self ).__init__( name, inNamespace, **params )
+        self.locallntf = locallntf 
+        self.flush = flush 
 	
-	def setManualConfig( self, intf ):
+    def setManualConfig( self, intf ):
 
-	        cfile = '/etc/network/interfaces'
-        	line = '\niface %s inet manual\n' % intf
-	        try:
-	            with open( cfile ) as f:
-        	        config = f.read()
-	        except IOError:
-	            config = ''
-	        if ( line ) not in config:
-	            info( '*** Adding "' + line.strip() + '" to ' + cfile + '\n' )
-	            with open( cfile, 'a' ) as f:
-	                f.write( line )
-	            self.cmd( 'service network-manager restart || netplan apply' )
+        cfile = '/etc/network/interfaces'
+        line = '\niface %s inet manual\n' % intf
+        try:
+            with open( cfile ) as f:
+                config = f.read()
+            except IOError:
+                config = ''
+            if ( line ) not in config:
+                info( '*** Adding "' + line.strip() + '" to ' + cfile + '\n' )
+                with open( cfile, 'a' ) as f:
+                    f.write( line )
+                self.cmd( 'service network-manager restart || netplan apply' )
 
     def config( self, **params ):
 
