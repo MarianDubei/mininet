ccc4fd1805ce80c630f5f5046e5f7db96518c9d1
sMaxym <02smaxym@gmail.com>
2021-06-04 16:59:59 +0300

    Added test descriptions

--- custom/test_hostonlynode.py
+++ custom/test_hostonlynode.py
@@ -6,6 +6,10 @@ import pytest
 prompt = 'mininet>'
 
 def test_setup():
+    '''
+    Setup of mininet network. If hostonlynode run successfully, then
+    prompt string value is expected to be met in the cli, i.e. cli_setup == 0
+    '''
     pytest.net = pexpect.spawn('python -m hostonlynode')
     try:
         cli_setup = pytest.net.expect(prompt)
@@ -13,6 +17,10 @@ def test_setup():
     assert cli_setup == 0
 
 def test_pingall():
+    '''
+    Ping all hosts in the network using the standart mininet API. Expected result is
+    parsed and percentage of failed ping requests is being accounted.
+    '''
     pytest.net.sendline( 'pingall' )
     pytest.net.expect ( '(\d+)% dropped' )
     print('(\d+)% dropped')
@@ -24,6 +32,10 @@ def test_pingall():
     ('h2', 'h1'),
 ])
 def test_host_ping_ip_reachable(hostname1, hostname2):
+    '''
+    Ping reciprocal hosts h1->h2 and h2->h1. Expected
+    failure rate is 0 (percent value).
+    '''
     pytest.net.expect(prompt)
     pytest.net.sendline('%s ping -c 1 %s' % (hostname1, hostname2))
     pytest.net.expect('(\d+)% packet loss')
@@ -34,6 +46,10 @@ def test_host_ping_ip_reachable(hostname1, hostname2):
     ('h1', '192.168.56.103'),
 ])
 def test_host_ping_ip_absent(hostname, ip):
+    '''
+    Ping unreachable host with IP address 192.168.56.103 with default
+    mininet API. As this IP is missing a failure rate is expected to be 100%
+    '''
     pytest.net.expect(prompt)
     pytest.net.sendline('%s ping -c 1 %s' % (hostname, ip))
     pytest.net.expect('(\d+)% packet loss')
@@ -45,9 +61,17 @@ def test_host_ping_ip_absent(hostname, ip):
     ('10.0.0.2'),
 ])
 def test_os_ping_host(host_ip):
+    '''
+    Ping network hosts with IPs 10.0.0.1 and 10.0.0.2
+    from the host OS. Required response is 0 (success)
+    '''
     response = os.system("ping -c 1 " + host_ip)
     assert response == 0
 
 def test_exit():
+    '''
+    Send quit command to mininet network and wait
+    for the shut down
+    '''
     pytest.net.sendline('quit')
     pytest.net.wait()
